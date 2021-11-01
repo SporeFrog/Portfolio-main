60de9790cec2ad72bf484d55ce64356348b283ad
Aaron <anaugustchill@gmail.com>
2021-11-01 09:53:53 -0400

    added footer, social links, fixed nav for mobile

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ function toggleNav(){
   if(document.querySelector("nav").classList.contains("hidden")){
     document.querySelector('nav').classList.remove("hidden");
     document.querySelector('#toggleButton').innerHTML = '<i class="fas fa-arrow-up"></i>';
-    // document.querySelector('#toggleButton').style="top:5rem;"
-  }else{
+document.querySelector("#toggleButton").classList.add("togglerSlide");  }else{
     document.querySelector("nav").classList.add("hidden");
     document.querySelector('#toggleButton').innerHTML = '<i class="fas fa-arrow-down"></i>';
-    // document.querySelector('#toggleButton').style="top:1rem;"
+    document.querySelector("#toggleButton").classList.remove("togglerSlide");
 
   }
 }
